88672ebbc3ebb00c0f76605d2a98e1700386a76f
sttronn <rishav.21m@gmail.com>
2021-02-11 00:38:54 +0530

    hello

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const webpack = require("webpack");
+const path = require("path");
+
+module.exports = {
+  entry: path.resolve(__dirname, "./src/index.js"),
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader","eslint-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"],
+  },
+  output: {
+    path: path.resolve(__dirname, "./public"),
+    filename: "bundle.js",
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+  devServer: { contentBase: path.resolve(__dirname, "./public"), hot: true },
+};
